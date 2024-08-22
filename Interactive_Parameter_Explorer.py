71c3b0011dbabe545ad6a83c0add468966dbb0b5
KSlater14 <166558588+KSlater14@users.noreply.github.com>
2024-08-22 16:41:56 +0000

    final changes

--- Interactive_Parameter_Explorer.py
+++ Interactive_Parameter_Explorer.py
@@ -9,6 +9,7 @@ import streamlit as st
 import numpy as np
 import requests
 import io
+import bokeh
 from bokeh.models import ColumnDataSource, LabelSet, HoverTool, Range1d
 from bokeh.plotting import figure
 from pyteomics import mzml, mass, parser
